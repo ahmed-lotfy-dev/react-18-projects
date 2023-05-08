3bf04e74c5d385927841ed8ca5caf05a76f95303
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-05-09 00:44:03 +0300

    commit main starter project to main branch

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-blue-400">
+        <h1 className="text-5xl font-bold underline text-blue-950 ">
+          Hello world!
+        </h1>{" "}
+      </div>
+    </>
+  );
+}
+
+export default App;
